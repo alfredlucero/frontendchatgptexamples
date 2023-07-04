55c958f0a82b0c975a6d61dfc5bf33dff311f727
Alfred Lucero <alfred.lucero@ucla.edu>
2023-07-03 21:18:14 -0700

    Playing with named actions and forms for login page

---
+++ sveltegpt/src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<form method="POST" action="?/login">
+  <label for="email">
+    Email
+    <input name="email" type="email" />
+  </label>
+  <label for="password">
+    Password
+    <input name="password" type="password" />
+  </label>
+  <button>Log in</button>
+  <button formaction="?/register">Register</button>
+</form>
