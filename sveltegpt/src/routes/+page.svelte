f2c4ac09c90553884bf951d3ed050e4fd81db1c8
Alfred Lucero <alfred.lucero@ucla.edu>
2023-07-03 18:57:38 -0700

    Added example sveltekit routes

--- sveltegpt/src/routes/+page.svelte
+++ sveltegpt/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Experimenting with OpenAI and Sveltekit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation for Sveltekit
+</p>
+<p><a href="/chatgpt">See ChatGPT example</a></p>
