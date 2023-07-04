cc3cc65652e8346c50b36d8307a2b1f4ff23804c
Alfred Lucero <alfred.lucero@ucla.edu>
2023-07-03 21:26:27 -0700

    Testing out form data and login action success state

--- sveltegpt/src/routes/login/+page.server.ts
+++ sveltegpt/src/routes/login/+page.server.ts
@@ -1,7 +1,21 @@
+/** @type {import('./$types').PageServerLoad}  */
+export async function load({ cookies }) {
+	const user = cookies.get('sessionId');
+
+	return { user };
+}
+
 /** @type {import('./$types').Actions } */
 export const actions = {
-	login: async (event) => {
-		// TODO: login
+	login: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const email = data.get('email');
+		const password = data.get('password');
+
+		// const user = await db.getUser(email, password);
+		cookies.set('sessionId', 'loggedInUserSessionId');
+
+		return { success: true };
 	},
 	register: async (event) => {
 		// TODO: register user
--- sveltegpt/src/routes/login/+page.svelte
+++ sveltegpt/src/routes/login/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+
+  /** @type {import('./$types').ActionData} */
+  export let form;
+</script>
+
+{#if form?.success}
+  <!-- Message is ephemeral and exists beacuse page was rendered in response to a form submission. It will vanish if user reloads -->
+  <p>Successfully logged in! Welcome back {data.user}!</p>
+{/if}
+
 <form method="POST" action="?/login">
   <label for="email">
     Email
