6e7d133ce0990e48db28c82efb643d8a7e1b75bf
Alfred Lucero <alfred.lucero@ucla.edu>
2023-07-05 21:55:11 -0700

    Added props example

--- sveltegpt/src/routes/tutorial1/+page.svelte
+++ sveltegpt/src/routes/tutorial1/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import Nested from './Nested.svelte';
   let name = 'Svelte';
-  let string = 'this string contains a <strong>strong HTML</strong>'
+  let string = 'this string contains a <strong>strong HTML</strong>';
+  let count = 0;
+  // Re-run whenever any of the referenced values change
+  $: doubled = count * 2;
+  
+  function increment() {
+    count += 1;
+  }
 </script>
 
 <style>
@@ -15,5 +22,11 @@
 <h1 data-name={name}>Hello {name}!</h1>
 
 <p>Test paragraph</p>
-<Nested />
+<Nested answer={17}/>
 <p>{@html string}</p>
+
+<button on:click={increment}>
+  Clicked {count}
+  {count === 1 ? 'time' : 'times'}
+</button>
+<p>{count} doubled is {doubled}</p>
--- sveltegpt/src/routes/tutorial1/Nested.svelte
+++ sveltegpt/src/routes/tutorial1/Nested.svelte
@@ -1,1 +1,5 @@
-<p>Another nested component</p>
+<script>
+  export let answer = 0;
+</script>
+
+<p>Another nested component with answer: {answer}</p>
